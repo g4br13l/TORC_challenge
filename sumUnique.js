abb93f045e78bf76eef623e3344aeb0973969ecf
Gabriel <gabrielnascimento.lima@portoseguro.com.br>
2023-10-25 02:07:58 -0300

    Performance optimized and Exception handling

--- sumUnique.js
+++ sumUnique.js
@@ -1,16 +1,22 @@
 
-// original function O(n^2)
-const sumUniqueOriginal = (nums) => {
-
+// Optimized function O(n)
+const sumUniqueOptimized = (arrayNums) => {
+  const numCount = {};
   let sum = 0;
 
-  nums.forEach(num => {
+  try {
+    arrayNums.forEach( num => {
+      if(typeof num !== 'number') throw 'type_not_accepted_exception';
+      numCount[num] = (numCount[num] || 0) + 1;
+    });
 
-    if (nums.indexOf(num) === nums.lastIndexOf(num)) {
-      sum += num;
-    }
+    arrayNums.forEach( num => {
+      if(numCount[num] === 1) sum += num;
+    });
 
-  });
+    return sum;
+  }
+  catch (err) { console.log(err); }
+};
 
-  return sum;
-}
+console.log( sumUniqueOptimized([1, 4, 4, 2, 5, 5, 5]) );
